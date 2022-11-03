74fb204cbbe11f5ad10f6bbf15711ea1378ebd65
ErrorCommander <kai_412@mail.ru>
2022-11-04 00:25:48 +0200

    Player input

--- Assets/GameResources/Scripts/Data/DataExtensions.cs
+++ Assets/GameResources/Scripts/Data/DataExtensions.cs
@@ -4,4 +4,5 @@ public static class DataExtensions
 {
     public static Vector3 AsVector3XZ(this Vector2 input) => new Vector3(input.x, 0, input.y);
     public static Vector3 ChangeY(this Vector3 input, float value = 0) => new Vector3(input.x, value, input.z);
+    public static Quaternion ChangeY(this Quaternion input, float value = 0) => new Quaternion(input.x, value, input.z, input.w);
 }
